12190b6c503016fab245e03680fe00251809ee57
flztjl <ssrsrfy.jl@gmail.com>
2023-10-26 10:17:46 +0800

    Add sessions create action (login)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,9 +2,12 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "create" do
-    assert_difference "User.count", 1 do
-      post "/users.json", params: { email: "test@test.com", password: "password", password_confirmation: "password" }
-      assert_response 201
-    end
+    post "/users.json", params: { email: "test@test.com", password: "password", password_confirmation: "password" }
+    post "/sessions.json", params: { email: "test@test.com", password: "password" }
+    assert_response 201
+  
+    data = JSON.parse(response.body)
+    assert_equal ["jwt", "email", "user_id"], data.keys
   end
+
 end
